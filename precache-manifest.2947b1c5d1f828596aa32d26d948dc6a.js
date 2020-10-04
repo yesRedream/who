2789e28f7bb3738d60a1ddc8e2396b599f7071df
yesRedream <yesredream@gmail.com>
2020-10-04 11:39:25 +0300

    Updates

--- precache-manifest.2947b1c5d1f828596aa32d26d948dc6a.js
+++ precache-manifest.2947b1c5d1f828596aa32d26d948dc6a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b4995a5ebf24f12dfaabef3f0e7c4b4d",
+    "revision": "0045002c9c2db5562635c8554a30e6cd",
     "url": "//yesredream.github.io/who/index.html"
   },
   {
-    "revision": "e5ac98913d547b61d092",
-    "url": "//yesredream.github.io/who/static/css/main.c677e5bc.chunk.css"
+    "revision": "8d254a77fdce44ddf9f7",
+    "url": "//yesredream.github.io/who/static/css/main.2234acba.chunk.css"
   },
   {
     "revision": "dda00a384161487ad999",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "//yesredream.github.io/who/static/js/2.a2feca09.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5ac98913d547b61d092",
+    "revision": "8d254a77fdce44ddf9f7",
     "url": "//yesredream.github.io/who/static/js/main.32c387f8.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//yesredream.github.io/who/precache-manifest.182dc2c51e710484c987e21d2aa04945.js"
+  "//yesredream.github.io/who/precache-manifest.2947b1c5d1f828596aa32d26d948dc6a.js"
 );
 
 self.addEventListener('message', (event) => {
