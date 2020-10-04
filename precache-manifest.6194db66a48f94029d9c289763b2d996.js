328120340f0fba2cdd1694db77cad49cf939d96e
yesRedream <yesredream@gmail.com>
2020-10-04 11:03:24 +0300

    Updates

---
+++ precache-manifest.6194db66a48f94029d9c289763b2d996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3143881013f7132c828836cfcec00fdf",
+    "url": "//yesredream.github.io/who/index.html"
+  },
+  {
+    "revision": "39a63ffce83f10cd21b8",
+    "url": "//yesredream.github.io/who/static/css/main.3d243bbb.chunk.css"
+  },
+  {
+    "revision": "dda00a384161487ad999",
+    "url": "//yesredream.github.io/who/static/js/2.a2feca09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//yesredream.github.io/who/static/js/2.a2feca09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a63ffce83f10cd21b8",
+    "url": "//yesredream.github.io/who/static/js/main.32c387f8.chunk.js"
+  },
+  {
+    "revision": "d0f91fe43b6bd5813fad",
+    "url": "//yesredream.github.io/who/static/js/runtime-main.3d4cfffa.js"
+  }
+]);
